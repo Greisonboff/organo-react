f835d3e2455633278136cf8a6ddd0036d85c2792
Greison Nicolau Böff <greisonboff@hotmail.com>
2023-05-30 20:04:03 -0300

    Update index.js

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -27,7 +27,7 @@ export default function Formulario(props) {
                 <h2>Preencha os dados para criar o card do colaborador</h2>
                 <CampoTexto valor={nome} aoAlterado={valor => setNome(valor)} obrigatorio={true} label={'Nome'} placeholder={'Digite seu nome'} />
                 <CampoTexto valor={cargo} aoAlterado={valor => setCargo(valor)} obrigatorio={true} label={'Cargo'} placeholder={'Digite seu cargo'} />
-                <CampoTexto valor={imagem} aoAlterado={valor => setImagem(valor)} label={'Imagem'} placeholder={'Digite o endereço da imagem'} />
+                <CampoTexto valor={imagem} aoAlterado={valor => setImagem(valor)} label={'Link de Imagem'} placeholder={'Digite o endereço da imagem'} />
                 <ListaSuspensa valor={time} aoAlterado={valor => setTime(valor)} required={true} label={'Time'} item={props.times} />
                 <Botao>
                     Criar card
